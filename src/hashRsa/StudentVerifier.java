d957cff86b6b7c55417e144e41b786c2eae61d4b
birmstf <birmstf@gmail.com>
2025-12-04 10:19:06 +0300

    hash and basic rsa

---
+++ src/hashRsa/StudentVerifier.java
@@ -0,0 +1,78 @@
+package hashRsa;
+
+public class StudentVerifier {
+
+    // Public RSA data (can be known by students) ---
+    // These must match the teacher side parameters.
+    private static final int N = 3233;   // modulus for RSA
+    private static final int E = 17;     // public KEY exponent
+
+    // TODO: Exercise 1
+    // STUDENT TASK (Exercise 1):
+    // Implement a simple hash function:
+    //  - Start with hash = 0
+    //  - For each character: hash = (hash + c) % 1000
+    //  - Return hash
+    // public static int simpleHash(String message) { ... }
+
+    public static int simpleHash(String message) {
+        int hash = 0;
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            hash = (hash + c) % 1000;
+        }
+        return hash;
+    }
+
+
+    // RSA: modular exponentiation (base^exponent mod mod) exponent aslında public key
+    // You may assume this method is correct and just use it later
+    // when verifying the digital signature.
+    public static int modPow(int base, int exponent, int mod) {
+        long result = 1;
+        long b = base % mod;
+        int e = exponent;
+
+        while (e > 0) {
+            if ((e & 1) == 1) {
+                result = (result * b) % mod;
+            }
+            b = (b * b) % mod;
+            e = e >> 1;
+        }
+
+        return (int) result;
+    }
+
+    // TODO: In a later exercise you will implement:
+
+
+    public static boolean verifyMessage(String message, int signature) {
+        // 1. Recompute the hash from the received message
+        boolean verifyM=false;
+        // 1. Mesajdan hash'i tekrar hesapla
+        int recomputedHash=simpleHash(message);
+
+        // 2. Recover the hash from the signature using public key
+        // 2. Öğretmenin imzasından hash'i geri çıkar
+        int recoveredHash=modPow(signature, E, N);
+
+        return recomputedHash == recoveredHash;
+      }
+
+
+        public static void main(String[] args) {
+        String receivedMessage = "Exam will be on Friday at 10:00.";
+        int receivedSignature = 330; // teacher's signature for the message above
+
+
+        System.out.println("=== STUDENT SIDE ===");
+        System.out.println("Received message  : " + receivedMessage);
+        System.out.println("Received signature: " + receivedSignature);
+        // In a later exercise, you will:
+
+        boolean isValid = verifyMessage(receivedMessage, receivedSignature);
+        System.out.println("Message:" + isValid);
+    }
+}
+
